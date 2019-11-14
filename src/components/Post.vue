9573ec5df5f9f6c386335132d2f86d14bfce32f9
Carlos Avila Aguilar <krloz1003@gmail.com>
2019-11-14 13:33:22 -0600

    Desarrollar el componente Post para iterar en el Blog

---
+++ src/components/Post.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-card color="indigo darken-4">
+        <v-img src="https://picsum.photos/id/11/500/300" aspect-ration="2.75" ></v-img>
+        <v-card-title primary-title>
+            <div>
+                <div class="headline">{{ post.title }}</div>
+                <span class="white--text" v-if="post.user">
+                    <v-icon>mdi-clipboard-account</v-icon>
+                    {{ post.user.name }}
+                </span>
+                <v-spacer v-if="detail"><br></v-spacer>
+                <span class="white--text" v-if="detail">{{ post.content }}</span>
+            </div>
+        </v-card-title>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-badge color="red" left>
+                <span slot="badge">{{ post.comments.length }}</span>
+                <v-icon large color="white">mdi-message-reply-text</v-icon>
+            </v-badge>
+
+            <v-btn v-if="!detail" @click="$router.push({name: 'post-detail', params: {id: post.id }})" icon>
+                <v-icon>mdi-arrow-right</v-icon>
+
+            </v-btn>
+
+
+        </v-card-actions>
+
+    </v-card>
+</template>   
+
+<script>
+export default {
+    name: "PostComponent",
+    props: {
+        post: {
+            type: Object,
+            required: true
+        },
+        detail: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+}
+</script>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -4,7 +4,7 @@
             <v-container fluid grid-list-md>
                 <v-layout row wrap>
                     <v-flex v-bind="{ ['xs4']: true }" v-for="post in posts" :key="post.id">
-                        post
+                        <post-component :post="post" />
                     </v-flex>
                 </v-layout>
             </v-container>
@@ -15,9 +15,13 @@
 
 <script>
 import Post from '@/database/models/Post'
+import PostComponent from '../components/Post';
 
 export default {
     name:'BlogPage',
+    components: {
+        PostComponent
+    },
     data () {
         return {
             posts: []
